1bfe9f0c637b21e7a413b46a1788966b3a5b95a2
Tamay <92460164+tamaygz@users.noreply.github.com>
2025-01-30 02:37:32 +0100

    8

--- lib/fluttermay.dart
+++ lib/fluttermay.dart
@@ -5,9 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
+
+/// widgets
+export 'widgets/icon_label_column.dart';
+
 /// List Extensions
 part 'list_extensions.dart';
 
 
-/// widgets
-export 'widgets/icon_label_column.dart';
