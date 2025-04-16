cab37e9bbfceadc9e61cb385012edc65dc4d9498
Tamay <92460164+tamaygz@users.noreply.github.com>
2025-04-16 10:21:56 +0200

    added tokenexpiringevent

--- lib/models/notification_event.dart
+++ lib/models/notification_event.dart
@@ -9,4 +9,23 @@ class NotificationEvent {
     required this.type,
     required this.message,
   }) : timestamp = DateTime.now();
+
+  @override
+  String toString() {
+    return 'NotificationEvent(type: $type, message: $message, sent_at: $timestamp)';
+  }
+}
+
+class JWTExpiringEvent extends NotificationEvent {
+  final DateTime expirationTime;
+
+  JWTExpiringEvent({
+    required String token,
+    required this.expirationTime,
+  }) : super(type: EventType.token, message: token);
+
+  @override
+  String toString() {
+    return 'JWTExpiringEvent(token: $message, expirationTime: $expirationTime, sent_at: $timestamp)';
+  }
 }
