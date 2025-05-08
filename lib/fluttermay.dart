1bf5733c121f7c8e39f2a8bec0916036e40fceb1
Tamay <92460164+tamaygz@users.noreply.github.com>
2025-05-08 08:53:58 +0200

    fix: correct export path for FluttermayNotifier to use the correct file

--- lib/fluttermay.dart
+++ lib/fluttermay.dart
@@ -1,7 +1,7 @@
 /// widgets
 export 'widgets/icon_label_column.dart';
 
-export 'src/fluttermay_notifier.bak';
+export 'src/fluttermay_notifier.dart';
 export 'src/server_notifier.dart';
 export 'models/notification_event.dart';
 export 'models/auth_token.dart';
